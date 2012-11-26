6712e17aac46fcb06817a301170bc32cdb14a000
akchandra.jobs@gmail.com <akchandra.jobs@gmail.com@bbd263e4-8b53-bb88-358c-001a096a60ae>
2012-11-26 08:23:47 +0000

    Created

---
+++ trunk/EMS.DAL/CommonDAL.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using EMS.Common;
+using EMS.DAL.DbManager;
+using EMS.Entity;
+
+namespace EMS.DAL
+{
+    public sealed class CommonDAL
+    {
+        private CommonDAL()
+        {
+        }
+
+        #region Common
+
+        /// <summary>
+        /// Saves the error log.
+        /// </summary>
+        /// <param name="userId">The user id.</param>
+        /// <param name="message">The message.</param>
+        /// <param name="stackTrace">The stack trace.</param>
+        /// <createdby>Amit Kumar Chandra</createdby>
+        /// <createddate>08/07/2012</createddate>
+        public static void SaveErrorLog(int userId, string message, string stackTrace)
+        {
+            string strExecution = "[admin].[uspSaveError]";
+
+            using (DbQuery oDq = new DbQuery(strExecution))
+            {
+                oDq.AddIntegerParam("@UserId", userId);
+                oDq.AddVarcharParam("@ErrorMessage", 255, message);
+                oDq.AddVarcharParam("@StackTrace", -1, stackTrace);
+                oDq.RunActionQuery();
+            }
+        }
+
+        #endregion
+    }
+}

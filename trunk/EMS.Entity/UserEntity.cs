2d6f4ba40b79cb18a0254c9c466699a07a4cc913
akchandra.jobs@gmail.com <akchandra.jobs@gmail.com@bbd263e4-8b53-bb88-358c-001a096a60ae>
2012-11-26 08:31:27 +0000

    Created

---
+++ trunk/EMS.Entity/UserEntity.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EMS.Common;
+using System.Data;
+
+namespace EMS.Entity
+{
+    public class UserEntity : IUser
+    {
+        #region IUser Members
+
+        public string Password
+        {
+            get;
+            set;
+        }
+
+        public string NewPassword
+        {
+            get;
+            set;
+        }
+
+        public string FirstName
+        {
+            get;
+            set;
+        }
+
+        public string LastName
+        {
+            get;
+            set;
+        }
+
+        public string UserFullName
+        {
+            get
+            {
+                return this.FirstName + " " + this.LastName;
+            }
+        }
+
+        public string EmailId
+        {
+            get;
+            set;
+        }
+
+        public char IsActive
+        {
+            get;
+            set;
+        }
+
+        #endregion
+
+        #region IBase<int> Members
+
+        public int Id
+        {
+            get;
+            set;
+        }
+
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        #endregion
+
+        #region ICommon Members
+
+        public int CreatedBy
+        {
+            get;
+            set;
+        }
+
+        public DateTime CreatedOn
+        {
+            get;
+            set;
+        }
+
+        public int ModifiedBy
+        {
+            get;
+            set;
+        }
+
+        public DateTime ModifiedOn
+        {
+            get;
+            set;
+        }
+
+        #endregion
+
+        #region Constructors
+
+        public UserEntity()
+        {
+
+        }
+
+        public UserEntity(DataTableReader reader)
+        {
+            this.Id = Convert.ToInt32(reader["UserId"]);
+            this.Name = Convert.ToString(reader["UserName"]);
+            this.FirstName = Convert.ToString(reader["FirstName"]);
+            this.LastName = Convert.ToString(reader["LastName"]);
+
+            if (reader["EmailId"] != DBNull.Value)
+                this.EmailId = Convert.ToString(reader["EmailId"]);
+
+            this.IsActive = Convert.ToChar(reader["Active"]);
+        }
+
+        #endregion
+    }
+}

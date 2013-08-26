1ecf7ef87c6c3d8bf7447cc36da973d600e381e6
rajensaha2005@gmail.com <rajensaha2005@gmail.com@bbd263e4-8b53-bb88-358c-001a096a60ae>
2013-08-26 18:21:17 +0000

    Wire-Frame

---
+++ EMS.WebApp/MasterModule/Export-charge-add-edit.aspx.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using EMS.Utilities;
+using EMS.BLL;
+
+namespace EMS.WebApp.MasterModule
+{
+    public partial class Export_charge_add_edit : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string[] Name = new string[5];
+            dgChargeRates.DataSource = Name;
+            dgChargeRates.DataBind();
+        }
+
+        protected void btnBack_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/MasterModule/Export-charge-list.aspx");
+        }
+
+        protected void rdbDestinationCharge_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (rdbDestinationCharge.SelectedValue == "1")
+                rdbFPOD.Enabled = true;
+            else
+                rdbFPOD.Enabled = false;
+        }
+
+        protected void rdbTerminalRequired_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //ddlFTerminal
+            RadioButtonList rdl = (RadioButtonList)sender;
+            TerminalSelection(rdl);
+            //if (rdbTerminalRequired.SelectedValue == "1")
+            //    TerminalSelection();
+        }
+
+        void PopulateDropDown(int Number, DropDownList ddl, int? Filter)
+        {
+            CommonBLL.PopulateDropdown(Number, ddl, Filter, 0);
+        }
+
+        private void TerminalSelection(RadioButtonList rdl)
+        {
+            GridViewRow Row = dgChargeRates.HeaderRow;
+            DropDownList ddlFTerminal = (DropDownList)Row.FindControl("ddlFTerminal");
+            //DropDownList ddlFLocation = (DropDownList)Row.FindControl("ddlFLocation");
+
+            if (rdl.SelectedItem.Value == "0")
+            {
+                if (ddlFTerminal.Items.Count > 0)
+                {
+                    //ddlFTerminal.SelectedIndex = 0;
+                    ddlFTerminal.Items.Clear();
+                }
+                ddlFTerminal.Enabled = false;
+            }
+            else
+            {
+                ddlFTerminal.Enabled = true;
+                ddlFTerminal.Items.Clear();
+                //if (Convert.ToInt32(ddlHeaderLocation.SelectedValue) > 0)
+                //    PopulateDropDown((int)Enums.DropDownPopulationFor.TerminalCode, ddlFTerminal, Convert.ToInt32(ddlHeaderLocation.SelectedValue));
+
+                //if (ddlHeaderLocation.SelectedValue == "-1")
+                //{
+                //    ListItem Li = new ListItem("ALL", "-1");
+                //    ddlFTerminal.Items.Insert(0, Li);
+                //}
+            }
+        }
+    }
+}
---
+++ EMS.WebApp/MasterModule/Export-charge-list.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EMS.WebApp.MasterModule
+{
+    public partial class Export_charge_list : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string[] Names = new string[5] { "A", "A", "A", "A", "A" };
+
+            gvwCharge.DataSource = Names;
+            gvwCharge.DataBind();
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/MasterModule/Export-charge-add-edit.aspx");
+        }
+    }
+}
